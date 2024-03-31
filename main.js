1628b79e4351360352e9b8e90fb1b92e4f5b077e
Your Name <you@example.com>
2024-04-01 00:01:32 +0500

    Add more object as you can

---
+++ main.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: 'YourList',
+            type: 'input',
+            message: "What you willing to add in your Todos ?"
+        },
+        {
+            name: 'addMore',
+            type: 'confirm',
+            message: "Do you willing to add more ?",
+            default: "false"
+        }
+    ]);
+    todos.push(addTask.YourList);
+    condition = addTask.addMore;
+    console.log(todos);
+}
